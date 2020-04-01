2046d5ed960b567bceffd3bf90d34c41344dbe70
yoga <yoga@Yoga460>
2020-04-01 19:30:47 -0400

    Added "This is a new test line" - TestOne.java

--- src/Test/TestOne.java
+++ src/Test/TestOne.java
@@ -5,6 +5,7 @@ public class TestOne {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World! We are Majestic 4!");
+		System.out.println("This is a new test line!!!!");
 	}
 
 }

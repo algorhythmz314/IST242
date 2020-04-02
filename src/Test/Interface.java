3bef835943ce39c75025a30f2ebbe7186ac205db
royaleblue <royaleblue@royales-mbp>
2020-04-01 20:27:10 -0400

    COMMIT

--- src/Test/Interface.java
+++ src/Test/Interface.java
@@ -4,7 +4,10 @@ public class Interface {
 
 	public static void main(String[] args) 
 	{
-	System.out.println("Welcome to Majestic 4's Inventory Management System!");
+	System.out.println("Welcome to Majestic 4's Inventory Management; System!");
+	System.out.println("Enter 1 to Add an Item");
+	System.out.println("Enter 2 to Remove an Item");
+	System.out.println("Enter 0 to Exit the Program");
 
 	}
 

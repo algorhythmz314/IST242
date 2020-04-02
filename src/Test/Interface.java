c484d6034091f852c9d3424f919ad8358f6902df
royaleblue <royaleblue@royales-mbp>
2020-04-01 20:53:40 -0400

    Console

--- src/Test/Interface.java
+++ src/Test/Interface.java
@@ -1,5 +1,7 @@
 package Test;
+import java.util.Scanner;
 
+//‎⁨Macintosh SSD⁩ ▸ ⁨Users⁩ ▸ ⁨royaleblue⁩ ▸ ⁨git⁩ ▸ ⁨IST242⁩ ▸ ⁨src⁩ ▸ ⁨Test⁩
 public class Interface {
 
 	public static void main(String[] args) 
@@ -8,7 +10,11 @@ public class Interface {
 	System.out.println("Enter 1 to Add an Item");
 	System.out.println("Enter 2 to Remove an Item");
 	System.out.println("Enter 0 to Exit the Program");
+	
+	
+	
+	Scanner consoleValue = new Scanner(System.in);
+	System.out.println("Enter 0, 1, or 2");
+	int numberEntered = consoleValue.nextInt();
 
-	}
-
-}
+}}
